474a6e2beaafef4219d223550808894b0b7a1883
sathish-25-A <sathisha25082000@gmail.com>
2024-08-26 15:13:27 +0530

    2a js

---
+++ lesson 2/lesson 2a.js
@@ -0,0 +1,9 @@
+// document.write(10 + 3 * 8 + 5);
+
+// document.write(18.5 + 2 * 7.5);
+
+// document.write((18.5 + 2 * 7.5) * 0.1);
+
+// document.write((18.5 + 2 * 7.5) * 0.2);
+
+// document.write(Math.floor(2.8));
