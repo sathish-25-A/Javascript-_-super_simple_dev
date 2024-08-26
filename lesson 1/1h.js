43a2d3e59722762fae023e6c0ba93d7aaab91a67
sathish-25-A <sathisha25082000@gmail.com>
2024-08-26 14:08:59 +0530

    1i js

--- lesson 1/1h.js
+++ lesson 1/1h.js
@@ -1,7 +1,11 @@
-let a = 10;
-let b = 8;
-let c = 20;
+// let a = 10;
+// let b = 8;
+// let c = 20;
 
-let d = a + b + c;
+// let d = a + b + c;
 
-document.write(d);
+// document.write(d);
+
+let a = 100;
+
+document.write(a - 20 - 50 + 200);
