e3f3efc4e3dbf91e8887348e1d772de693dd78b5
sathish-25-A <sathisha25082000@gmail.com>
2024-08-26 17:20:33 +0530

    cart list

--- lesson 3/3k.js
+++ lesson 3/3k.js
@@ -1,1 +1,11 @@
-document.write(`items(${2 + 2}): $${(2 * 2095 + 2 * 799) / 100}`);
+document.write(`items(${2 + 2}): $${(2 * 2095 + 2 * 799) / 100}
+`);
+document.write(`Shipping & handling : ($${2 * 4.99})`);
+
+document.write(`Total Before tax :$${(2 * 2095 + 2 * 799 + 499 + 499) / 100}`);
+
+document.write(
+  `Estimated tax (10%):  ${
+    Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100
+  } `
+);
