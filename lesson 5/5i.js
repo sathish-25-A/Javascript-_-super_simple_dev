fed11a2d43ebf5312a86f5a1e3bb28c9b26cce92
sathish-25-A <sathisha25082000@gmail.com>
2024-08-27 11:40:46 +0530

    5i & 5j html and js

--- lesson 5/5i.js
+++ lesson 5/5i.js
@@ -6,7 +6,9 @@ let Plus_4 = document.getElementById("+4");
 let Plus_5 = document.getElementById("+5");
 let rest = document.getElementById("rest");
 let para = document.getElementById("para");
-
+let remove_1 = document.getElementById("-1");
+let remove_2 = document.getElementById("-2");
+let remove_3 = document.getElementById("-3");
 
 let cart = 0;
 show.onclick = function () {
@@ -32,4 +34,12 @@ rest.onclick = function () {
   cart = 0;
   document.getElementById("para").innerHTML = ` Quantity : ${cart}`;
 };
-
+remove_1.onclick = function () {
+  document.getElementById("para").innerHTML = ` Quantity :  ${(cart -= 1)}`;
+};
+remove_2.onclick = function () {
+  document.getElementById("para").innerHTML = ` Quantity :  ${(cart -= 2)}`;
+};
+remove_3.onclick = function () {
+  document.getElementById("para").innerHTML = ` Quantity :  ${(cart -= 3)}`;
+};
