557f729da74013d44eaddca8b440bda890632630
sathish-25-A <sathisha25082000@gmail.com>
2024-08-27 10:36:56 +0530

    5c to 5g html and js

---
+++ lesson 5/5c.js
@@ -0,0 +1,17 @@
+let coffee = 5;
+
+let bagels = 3;
+
+let soup = 9;
+
+let cost;
+
+cost = coffee + 2 * bagels + soup;
+
+let tax = cost * 0.1;
+
+let totalcost = cost + tax;
+
+console.log(
+  `Cost of food : $${cost} \n tax(10%) :$${tax} \n total cost : $${totalcost}`
+);
