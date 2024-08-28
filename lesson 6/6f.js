0474426b250d30af0bf4196c08f10df9db737842
sathish-25-A <sathisha25082000@gmail.com>
2024-08-28 10:13:56 +0530

    6f to 6i  html & js

--- lesson 6/6f.js
+++ lesson 6/6f.js
@@ -1,8 +1,18 @@
 let result = document.getElementById("para");
+let result1 = document.getElementById("para1");
 let random = Math.random();
-
-if (random <= 0.5) {
+let answer;
+if (random < 0.5) {
+  answer = "head";
   result.textContent = "Head";
 } else {
+  answer = "Tails";
   result.textContent = "Tails";
+}
+let guess = "head";
+
+if (guess === answer) {
+  result1.textContent = "you win";
+} else {
+  result1.textContent = "you lose";
 }
