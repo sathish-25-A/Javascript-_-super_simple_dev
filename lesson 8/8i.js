deec608ecec8605b040fd3ed42c7234aa77924ec
sathish-25-A <sathisha25082000@gmail.com>
2024-08-29 18:17:00 +0530

    8j html & js

--- lesson 8/8i.js
+++ lesson 8/8i.js
@@ -9,9 +9,20 @@
 
 // result1.textContent = guess === answer ? "you win" : "you lose";
 
+let para = document.getElementById("para");
+let score = {
+  win: 0,
+  losses: 0,
+};
+
 function playgame(guess) {
   let random = Math.random();
   let result = random < 0.5 ? "head" : "Tail";
-
+  if (guess === result) {
+    score.win++;
+  } else {
+    score.losses--;
+  }
   alert(guess === result ? "you win" : "You lose");
+  para.textContent = JSON.stringify(score);
 }
