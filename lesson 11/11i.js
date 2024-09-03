36bf1e186f652401362e540bd13627f9fe5bec40
sathish-25-A <sathisha25082000@gmail.com>
2024-09-03 10:11:14 +0530

    12a html & js

--- lesson 11/11i.js
+++ lesson 11/11i.js
@@ -1,9 +1,20 @@
 function addOne(array, num) {
-    const answer = [];
-  
-    for (let i = 0; i < array.length; i++) {
-      answer.push(array[i] + num);
-    }
-  
-    return answer;
+  const answer = [];
+
+  for (let i = 0; i < array.length; i++) {
+    answer.push(array[i] + num);
   }
+
+  return answer;
+
+
+if (value < result.min) {
+  result.min = value;
+}
+
+if (value > result.max) {
+  result.max = value;
+}
+}
+
+return result;
