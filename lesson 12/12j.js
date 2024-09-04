4d93135f3c4505e5832d1732502dd4c338c3bd07
sathish-25-A <sathisha25082000@gmail.com>
2024-09-04 10:56:01 +0530

    12j js update

--- lesson 12/12j.js
+++ lesson 12/12j.js
@@ -1,5 +1,7 @@
-const fun1 = (value1, value2) => {
-  return value1 * value2;
-};
+// const fun1 = (value1, value2) => {
+//   return value1 * value2;
+// };
 
-console.log(fun1(4,4));
+const fun1 = (val1, val2) => val1 * val2;
+
+console.log(fun1(4, 4));
