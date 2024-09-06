f2f9f50a452da1bc2afd1eb60ab2a36f22ccef40
sathish-25-A <sathisha25082000@gmail.com>
2024-09-06 17:35:04 +0530

    update obj

--- lesson 12/obj.js
+++ lesson 12/obj.js
@@ -1,10 +1,10 @@
-
-const obj3 ={
-    name : "hari",
-    age : 20,
-    toString :function(){
-        return `name:${this.name} age:${this.age} ages`;
-    }
+const obj3 = {
+  name: "hari",
+  age: 20,
+  fuc: function () {
+    return `name:${this.name} age:${this.age} ages`;
+  }
 };
+document.querySelector(".para").innerHTML = obj3.fuc();
 
-console.log(obj3.toString());
+// console.log(obj3.toString());
