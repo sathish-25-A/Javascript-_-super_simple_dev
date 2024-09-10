c1f347f30ca42994f77140e76d02295d9ba9f061
sathish-25-A <sathisha25082000@gmail.com>
2024-09-10 10:15:50 +0530

    update obj2

--- obj/obj2.js
+++ obj/obj2.js
@@ -3,11 +3,34 @@ let product = {
   price: 500,
   category: "Electronics",
   quantity: 100,
-  sell(quantity){
-
-  },
-  restock(quantity){
-
-  }
-
 };
+
+function val(name, price, category, quantity) {
+  this.name = name;
+  this.price = price;
+  this.category = category;
+  this.quantity = quantity;
+
+  let totalsolid = 0;
+
+  this.price = function (sellquantity) {
+    if (this.quantity < sellquantity) {
+      console.log(` insufficient ${this.name}`);
+    } else {
+      this.quantity -= sellquantity;
+      totalsolid += sellquantity;
+      console.log(`${sellquantity} solid ${this.name}`);
+      return totalsolid;
+    }
+
+    this.gettotalsolid = function () {
+      return totalsolid;
+    };
+  };
+}
+
+let laptop = new val("Laptop", 500, "Electronics", 100);
+
+laptop.price(20);
+laptop.price(90);
+console.log(laptop.gettotalsolid());
