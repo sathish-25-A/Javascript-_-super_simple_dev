53f285bf187f5ba3d1fd9d9fa6e05f8af22465d2
sathish-25-A <sathisha25082000@gmail.com>
2024-09-11 15:54:07 +0530

    obj 10

---
+++ obj/onj10.js
@@ -0,0 +1,13 @@
+let calculator = {
+  add(a, b) {
+    return a + b;
+  },
+
+  sub(a, b) {
+    return a - b;
+  },
+};
+
+
+console.log(calculator.add(10, 5)); 
+console.log(calculator.sub(10, 5));
