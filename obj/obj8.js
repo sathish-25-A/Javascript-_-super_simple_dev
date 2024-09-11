32b04ad13eab1717b018714dfbf07cd74328e1c1
sathish-25-A <sathisha25082000@gmail.com>
2024-09-11 15:28:47 +0530

    obj 8

---
+++ obj/obj8.js
@@ -0,0 +1,21 @@
+let employees = [
+  {
+    name: "john",
+    age: "30",
+    position: "Developer",
+  },
+  {
+    name: "jane",
+    age: "25",
+    position: "Designer",
+  },
+  {
+    name: "johns",
+    age: "31",
+    position: "Developer",
+  },
+];
+
+employees.forEach((employees) => {
+  console.log(`${employees.name} is a ${employees.position}`);
+});
