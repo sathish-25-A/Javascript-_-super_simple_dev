69723199101d51bb9a328dc6974970eb492fdc8b
sathish-25-A <sathisha25082000@gmail.com>
2024-09-13 11:51:45 +0530

    fun1

---
+++ Function/fun1.js
@@ -0,0 +1,25 @@
+//hoisting
+
+//variable
+
+let fun = "sathish";
+fun = "sathish a";
+console.log(fun);
+
+//function
+
+//declare
+
+sat();
+
+function sat() {
+  console.log("hi");
+}
+
+//expression
+
+let new1 = function sat1() {
+  console.log("h11");
+};
+
+new1();
